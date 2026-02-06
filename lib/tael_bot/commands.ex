2b0585622e6f5a37a0d2020e2b89dcf4048e1b43
Nax <4265415+Nax@users.noreply.github.com>
2026-02-06 21:26:09 +0100

    Working command management

--- lib/tael_bot/commands.ex
+++ lib/tael_bot/commands.ex
@@ -10,9 +10,34 @@ defmodule TaelBot.Commands do
   end
 
   def create(attrs) do
-    %Command{}
-    |> Command.changeset(attrs)
-    |> Repo.insert()
+    try do
+      %Command{}
+      |> Command.changeset(attrs)
+      |> Repo.insert()
+    rescue
+      e -> {:error, e}
+    end
+  end
+
+  def update(attrs) do
+    case Repo.update_all(from(c in Command, where: c.name == ^attrs.name), set: [value: attrs.value]) do
+      {0, _} -> {:error, :not_found}
+      _ -> {:ok, nil}
+    end
+  end
+
+  def rename(old_name, new_name) do
+    case Repo.update_all(from(c in Command, where: c.name == ^old_name), set: [name: new_name]) do
+      {0, _} -> {:error, :not_found}
+      _ -> {:ok, nil}
+    end
+  end
+
+  def delete(name) do
+    case Repo.delete_all(from(c in Command, where: c.name == ^name)) do
+      {0, _} -> {:error, :not_found}
+      _ -> {:ok, nil}
+    end
   end
 
   def list_names() do
--- lib/tael_bot/commands/handlers/cmd.ex
+++ lib/tael_bot/commands/handlers/cmd.ex
@@ -1,5 +1,99 @@
 defmodule TaelBot.Commands.Handlers.Cmd do
-  def run(_msg, _arg) do
-    IO.puts("Hello from !cmd")
+  import TaelBot.Commands.Helpers
+
+  @handlers %{
+    "add" => :handle_add,
+    "edit" => :handle_edit,
+    "rename" => :handle_rename,
+    "delete" => :handle_delete,
+  }
+
+  defp normalize(str) do
+    str
+    |> String.trim()
+    |> String.downcase()
+    |> case do
+      "!" <> cmd -> cmd
+      cmd -> cmd
+    end
+  end
+
+  defp command_name(str) do
+    cmd = normalize(str)
+    if String.match?(cmd, ~r/^[a-zA-Z0-9_-]{1,64}$/) do
+      {:ok, cmd}
+    else
+      {:error, "Invalid command name"}
+    end
+  end
+
+  def run(msg, arg) do
+    require_role!(msg, :moderator)
+    case String.split(arg, " ", parts: 2) do
+      [subcmd, subargs] ->
+        case Map.get(@handlers, subcmd) do
+          nil -> reply(msg, "Unknown subcommand")
+          handler -> apply(__MODULE__, handler, [msg, subargs])
+        end
+      _ ->
+        reply(msg, "Available subcommands: " <> Enum.join(Map.keys(@handlers), ", "))
+    end
+  end
+
+  def handle_add(msg, arg) do
+    case String.split(arg, " ", parts: 2) do
+      [name, value] ->
+        case command_name(name) do
+          {:ok, name} ->
+            case TaelBot.Commands.create(%{name: name, value: value}) do
+              {:ok, _cmd} -> reply(msg, "Command added successfully")
+              {:error, %{type: :unique}} -> reply(msg, "Error: A command with that name already exists")
+              {:error, _} -> reply(msg, "Error: Unknown error")
+            end
+          {:error, reason} ->
+            reply(msg, reason)
+        end
+      _ ->
+        reply(msg, "Usage: !cmd add <name> <value>")
+    end
+  end
+
+  def handle_edit(msg, arg) do
+    with [name, value] <- String.split(arg, " ", parts: 2),
+         {:ok, name} <- command_name(name) do
+      case TaelBot.Commands.update(%{name: name, value: value}) do
+        {:ok, _} -> reply(msg, "Command updated successfully")
+        {:error, :not_found} -> reply(msg, "Error: Command not found")
+        {:error, _} -> reply(msg, "Error: Unknown error")
+      end
+    else
+      _ -> reply(msg, "Usage: !cmd edit <name> <value>")
+    end
+  end
+
+  def handle_rename(msg, arg) do
+    with [old_name, new_name] <- String.split(arg, " ", parts: 2),
+         {:ok, old_name} <- command_name(old_name),
+         {:ok, new_name} <- command_name(new_name) do
+      case TaelBot.Commands.rename(old_name, new_name) do
+        {:ok, _} -> reply(msg, "Command renamed successfully")
+        {:error, :not_found} -> reply(msg, "Error: Command not found")
+        {:error, _} -> reply(msg, "Error: Unknown error")
+      end
+    else
+      _ -> reply(msg, "Usage: !cmd rename <old_name> <new_name>")
+    end
+  end
+
+  def handle_delete(msg, arg) do
+    with {:ok, name} <- command_name(arg) do
+      case TaelBot.Commands.delete(name) do
+        {:ok, _} -> reply(msg, "Command deleted successfully")
+        {:error, :not_found} -> reply(msg, "Error: Command not found")
+        {:error, _} -> reply(msg, "Error: Unknown error")
+      end
+    else
+      _ -> reply(msg, "Usage: !cmd delete <name>")
+    end
   end
 end
